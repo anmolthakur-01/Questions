077cb0ca3d776cd269b160faca5b13b6e36c8515
Anmol Thakur <at2314541@gmail.com>
2025-02-14 10:10:50 +0530

    first operation complete

---
+++ array2.js
@@ -0,0 +1,16 @@
+// Create an array of songs name and do the following operations:
+// 1. Print the entire array with its type and length
+// 2. Add two new songs at begining
+// 3. remove last song
+
+songs = [
+  "Shape of You",
+  "Blinding Lights",
+  "Rolling Deep",
+  "Perfect",
+  "Uptown Funk",
+];
+
+console.log("The entire array : ", songs);
+console.log("Type of an array : ", typeof songs);
+console.log("Length of an array : ", songs.length);
