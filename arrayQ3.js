83607363396518663b532105425bcad674e215db
Anmol Thakur <at2314541@gmail.com>
2025-02-14 12:53:25 +0530

    question three complete

---
+++ arrayQ3.js
@@ -0,0 +1,14 @@
+// Create an array of objects named restaurant having keys restaurant name, rating, and description
+// 1. using for in traverse array
+
+restaurant = [
+  {
+    restaurantName: "Burger Haven",
+    rating: 4.3,
+    description: "Juicy gourmet burgers with a variety of toppings.",
+  },
+];
+
+for (y in restaurant) {
+  console.log(y, restaurant[0]);
+}
